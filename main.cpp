c6647bf0c95987ec6f7897a490784897adae4320
davidohalloran2003 <davidohalloran2003@gmail.com>
2024-06-15 10:45:51 +0000

    Lab 2 Finbonachi Summer 2024

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "mbed.h"
+
+/*
+next term is the sum of the previous two terms.
+The first two terms of the Fibonacci sequence are 0 folowed by 1.
+*/
+
+// main() runs in its own thread in the OS
+
+int t_1 = 0;
+int t_2 = 1;
+int next_term = 0;
+
+int max_terms = 10;
+
+int main()
+{
+
+ printf("Finbonachi series: %d,%d, \r\n", t_1, t_2);
+    for (i=0; i<max_terms; i++){ 
+    next_term = t_1 + t_2;
+    printf("%d, ", next_term);
+    t_1 = t2;
+    t2 = next_term;
+    }
+
+ printf("r\n")
+    printf(bbbbbbbb);
+
+    while (true) {
+
+    }
+}
