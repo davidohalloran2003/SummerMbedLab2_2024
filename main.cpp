6efb40db89cfe4e8ac7b1470c8a0cb942f2b4cc8
davidohalloran2003 <davidohalloran2003@gmail.com>
2024-06-17 12:55:46 +0000

    Lab2_Loop 2_Finbonachi 3

--- main.cpp
+++ main.cpp
@@ -8,15 +8,13 @@ Three control elements for the loop.
 First one is initialisation. Generally this is an integer.  
 Second one is the stop. i<=5 for example. 
 Finally is Control. This indicates how the loop is being controlled. i++ for example. Incrementally increasing the value. 
-So, in this case it will start off at zero, and will incrementally increase the vale of i after every loop, checks if it is less than 5, and continues. This example will print Loop 1 - Loop 4 as it has been coded to return a value up to <5. 
-Print result in this case 
-Finbonachi series: 0,1, 
-loop 0 
-loop 1 
-loop 2 
-loop 3 
-loop 4 
-loop 5 
+
+For the below coding, max_terms means to print the max amount of terms indicated in the variable.
+For the looping next_term will print the next term 
+
+Finbonachi series: 0,1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
+
+
 
 
 */
@@ -27,15 +25,22 @@ int t_1 = 0;
 int t_2 = 1;
 int next_term = 0;
 
-int max_terms = 10;
+int max_terms = 12;
 
 int main()
 {
 
- printf("Finbonachi series: %d,%d, \r\n", t_1, t_2);
-    for (int i=0; i<=5; i++){ 
-        printf("loop %d \r\n", i);
+ printf("Finbonachi series: %d,%d,", t_1, t_2);
+    for (int i=0; i<max_terms; i++){ 
+    next_term = t_1 + t_2;
+    printf("%d, ", next_term);
+        t_1 = t_2;
+        t_2 = next_term;
+   
     }
+
+ printf("\r\n");
+    
 
     while (true) {
 
